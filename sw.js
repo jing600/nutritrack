087400d4952ecfea1d34be3afc563bffd9806666
jing600 <18810921920@163.com>
2026-08-10 13:52:55 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,77 +1,8 @@
-const CACHE_NAME = "nutritrack-v2";
-
-const APP_FILES = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-// 安装：缓存基础文件
-self.addEventListener("install", event => {
-  self.skipWaiting();
-
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(APP_FILES);
-    })
-  );
-});
-
-// 激活：删除旧版本缓存
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      );
-    }).then(() => self.clients.claim())
-  );
-});
-
-// 请求：网页优先联网，失败才用缓存
-self.addEventListener("fetch", event => {
-  const request = event.request;
-
-  // 只处理 GET
-  if (request.method !== "GET") return;
-
-  // 页面导航：网络优先
-  if (request.mode === "navigate") {
-    event.respondWith(
-      fetch(request)
-        .then(response => {
-          const copy = response.clone();
-
-          caches.open(CACHE_NAME).then(cache => {
-            cache.put("./index.html", copy);
-          });
-
-          return response;
-        })
-        .catch(() => {
-          return caches.match("./index.html");
-        })
-    );
-
-    return;
-  }
-
-  // 其他静态资源：先网络，失败再缓存
-  event.respondWith(
-    fetch(request)
-      .then(response => {
-        const copy = response.clone();
-
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(request, copy);
-        });
-
-        return response;
-      })
-      .catch(() => caches.match(request))
-  );
+const CACHE='nutritrack-v1';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  const url=new URL(e.request.url);
+  if(url.origin===location.origin){e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res})));}
 });
